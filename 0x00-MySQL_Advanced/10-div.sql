0aa37aef0f75efa92fe1f303bee268cc98ee65de
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-19 01:21:36 +0100

    10. Safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,22 @@
+-- perevents DivsionEroor and return 0 instead
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(
+    a INT,
+    b INT
+)
+RETURNS FLOAT
+BEGIN
+    DECLARE result FLOAT;
+
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+
+    RETURN result;
+END $$
+
+DELIMITER ;
