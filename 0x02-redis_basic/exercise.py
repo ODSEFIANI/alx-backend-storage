89842c5575b9b1e3d16d9970cd767a965be901df
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-25 22:54:45 +0100

    anotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -93,3 +93,14 @@ def replay(method: Callable):
 
 if __name__ == "__main__":
     pass  # You can add test cases or run the main program here
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
